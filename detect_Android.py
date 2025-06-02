3a5710cc27113f482789b6bd1eb19b3005d7fb0b
brian <brianarguello.vga>
2025-06-02 17:17:02 -0300

    Aun falta, tengo sueño jajaja

--- detect_Android.py
+++ detect_Android.py
@@ -1,3 +1,4 @@
+from os.path import exists
 from re import sub
 import pyudev
 import subprocess
@@ -11,24 +12,33 @@ def android(device):
     model = device.get("ID_MODEL_ID")
     return model == "ff40"
 
-def launch_Rofi():
-    subprocess.run(["./hola.sh"])
+def launch_Rofi(nombre):
+    resultado = subprocess.run(["./rofip.sh", nombre],
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE,
+                   text=True,
+                   check=True 
+                   )
+    if resultado.stderr:
+        exit()
+
+def crearCarpeta(ruta, nombre):
+    respaldo = Path(f"{ruta}/{nombre}")
+    respaldo.mkdir(exist_ok=True)
 
 def scan_Android():
     context = pyudev.Context()
     monitor = pyudev.Monitor.from_netlink(context)
     monitor.filter_by(subsystem="usb")
     monitor.start
-
-
-    
     for action, device in monitor:
         if action == "add" and android(device):
-            print(get_Name_Android(device))
-            launch_Rofi()
+            nombre = get_Name_Android(device)
+            launch_Rofi(nombre)
+            crearCarpeta(ruta, nombre)
 
 
-          
+ruta = "/home/idksa_script/Documentos" 
 
 if __name__ == "__main__":
     scan_Android()
